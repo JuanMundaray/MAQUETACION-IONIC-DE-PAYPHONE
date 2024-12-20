5a76b848cd44ced3c6478c4c0be7142f02a69426
Juan Mundaray <juanfmundaray2840@gmail.com>
2024-12-19 21:01:04 -0400

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'MAQUETACION_PAYPHONE_ADMIN_DASHBOARD_IONIC',
+  webDir: 'www'
+};
+
+export default config;
